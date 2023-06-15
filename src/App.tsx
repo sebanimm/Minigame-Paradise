1dd6d0028a59f46efc0c2b6fa8e33eb1bdc41ce1
sebanimm <gks32114221@gmail.com>
2023-06-15 10:36:44 +0900

    ADD : globalstyle, router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Main from "@/pages/Main";
+
 const App = () => {
-	return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyle.ts";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <GlobalStyle />
+    <App />
+  </BrowserRouter>,
+);
