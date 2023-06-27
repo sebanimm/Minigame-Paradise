5a1d13b0ca1c2239c8fda2bf38e8713be20ea51d
sebanimm <gks32114221@gmail.com>
2023-06-28 08:38:57 +0900

    ADD : Rank

---
+++ src/components/Rank/style.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Info = styled.div`
+  border: 2px solid black;
+  width: 990px;
+  min-height: 120px;
+  margin: 20px;
+  border-radius: 20px;
+  display: flex;
+`;
+
+export const Rating = styled.div`
+  width: 10%;
+  font-size: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Name = styled.div`
+  width: 50%;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Score = styled.div`
+  width: 20%;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
