7e870926812f6b7d192ae39b8587141471f34b10
sebanimm <gks32114221@gmail.com>
2023-06-28 16:04:35 +0900

    ADD : index.ts

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import Arrow from "@/assets/arrow.svg";
+import BlockGame from "@/assets/blockGame.svg";
+import Targetgame from "@/assets/targetGame.svg";
+import InProgress from "@/assets/in-progress.svg";
+import FlyFly from "@/assets/flyfly.svg";
+
+export { Arrow, BlockGame, Targetgame, InProgress, FlyFly };
