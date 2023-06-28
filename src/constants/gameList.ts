fff3d7607804e81a1d05498af39c3aa7e9e62e69
sebanimm <gks32114221@gmail.com>
2023-06-28 16:05:02 +0900

    ADD : 'link' property

--- src/constants/gameList.ts
+++ src/constants/gameList.ts
@@ -1,47 +1,54 @@
-import Construction from "@/assets/construction.svg";
-import BlockGame from "@/assets/blockGame.svg";
-import TargetGame from "@/assets/targetGame.svg";
+import { GameListType } from "@/types/constants.type";
+import * as I from "@/assets";
 
-const gameList = [
+const gameList: GameListType[] = [
   {
     "id": 1,
     "name": "날려날려",
-    "src": TargetGame,
+    "src": I.InProgress,
+    "link": "",
   },
   {
     "id": 2,
     "name": "막아막아",
-    "src": BlockGame,
+    "src": I.InProgress,
+    "link": "",
   },
   {
     "id": 3,
     "name": "피해피해",
-    "src": Construction,
+    "src": I.InProgress,
+    "link": "",
   },
   {
     "id": 4,
     "name": "파닥파닥",
-    "src": Construction,
+    "src": I.FlyFly,
+    "link": "/flyfly",
   },
   {
     "id": 5,
     "name": "뿌셔뿌셔",
-    "src": Construction,
+    "src": I.InProgress,
+    "link": "",
   },
   {
     "id": 6,
     "name": "달려달려",
-    "src": Construction,
+    "src": I.InProgress,
+    "link": "",
   },
   {
     "id": 7,
     "name": "높이높이",
-    "src": Construction,
+    "src": I.InProgress,
+    "link": "",
   },
   {
     "id": 8,
     "name": "그려그려",
-    "src": Construction,
+    "src": I.InProgress,
+    "link": "",
   },
 ];
 
